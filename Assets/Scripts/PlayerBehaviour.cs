8e6ae906bca07ac48c7ca60e72c3811ccc479e05
Jacqueline Alves <jacque.alves.b@gmail.com>
2023-04-03 18:47:55 -0300

    creates game project

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private int touching = 0;
+    private float dirX;
+
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0f, 0f, dirX * touching);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("rock"))
+        {
+            touching = 1;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("rock"))
+        {
+            touching = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/RockMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D rb;
+    private float dirX;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            dirX = Input.GetAxis("Horizontal") * speed;
+            transform.Rotate(0f, 0f, dirX);
+        }
+        else
+        {
+            dirX = Input.acceleration.x * speed;
+        }
+        
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -9f, 9f), transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, 0f);
+    }
+}

c16318cc7ed903ac05fedd2bfe7fa75bb6979c83
afss2 <andreferreira72@gmail.com>
2023-05-11 22:21:52 -0300

    Added pause menu

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -7,9 +7,19 @@ public class StartGameButton : MonoBehaviour
 {
 
     public int gameStartScene;
+    public GameObject pauseMenu;
 
     public void StartGame()
     {
         SceneManager.LoadScene(gameStartScene);
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        pauseMenu.SetActive(false);
+        SceneManager.LoadScene(gameStartScene);
+        Time.timeScale = 0;
     }
 }
